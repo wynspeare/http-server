63e618e195c5ef1e5de9a9acd3f69c8ea3e52c14
wynspeare <caroline@8thlight.com>
2019-09-03 15:17:46 -0500

    Modified travis.yml file

--- acceptance-tests.sh
+++ acceptance-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 gem install bundler
-wget https://github.com/wynspeare/http-server-spec/archive/master.tar.gz
+wget https://github.com/wynspeare/http_server_spec/archive/master.tar.gz
 tar -xzvf master.tar.gz
 cd http-server-spec-master && bundle install
 bundle exec spinach
