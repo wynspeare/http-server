2546e3ae477bcc7f9d9be73a3491cecc5633fe51
wynspeare <caroline@8thlight.com>
2019-09-18 12:52:44 -0500

    Handler builds response with received body

--- src/test/server/HandlerTest.java
+++ src/test/server/HandlerTest.java
@@ -40,4 +40,32 @@ public class HandlerTest {
 
     assertEquals("HTTP/1.1 200 OK\r\n", response.getStatusLine());
   }
+
+  @Test
+  public void handlerChecksIfRequestIsAPOST() {
+    Request request = new Request("POST /random_path HTTP/1.1");
+    Handler handler = new Handler(request);
+
+    assertTrue(handler.isPOSTRequest());
+  }
+
+  @Test
+  public void handlerBuildsTheCorrectResponseStatusLineFromAPOSTRequest() {
+    String incomingRequest = "POST /simple_get HTTP/1.1\n" +
+            "Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3\n" +
+            "Accept: */*\n" +
+            "User-Agent: Ruby\n" +
+            "Connection: close\n" +
+            "Host: 127.0.0.1:5000\n" +
+            "Content-Length: 32\n" +
+            "Content-Type: application/x-www-form-urlencoded\n" +
+            "\r\n" +
+            "some body that could be anything";
+    Request request = new Request(incomingRequest);
+
+    Handler handler = new Handler(request);
+    Response response = handler.buildResponse();
+
+    assertEquals("HTTP/1.1 200 OK\r\nsome body that could be anything", response.getStatusLine());
+  }
 }
--- src/test/server/ResponseTest.java
+++ src/test/server/ResponseTest.java
@@ -2,6 +2,7 @@ package server;
 
 import HTTPcomponents.StatusCode;
 import org.junit.Test;
+import server.request.Request;
 
 import static org.junit.Assert.*;
 
@@ -25,8 +26,16 @@ public class ResponseTest {
   @Test
   public void responseBuildsResponseWithReceivedBody() {
     Response response = new Response();
-    response.build(StatusCode.OK);
 
-    assertEquals("HTTP/1.1 200 OK\r\n", response.getStatusLine());
+    String incomingRequest = "POST /simple_get HTTP/1.1\n" +
+            "Content-Length: 32\n" +
+            "Content-Type: application/x-www-form-urlencoded\n" +
+            "\r\n" +
+            "some body that could be anything";
+    Request request = new Request(incomingRequest);
+
+    response.build(StatusCode.OK, request.getRequestBody());
+
+    assertEquals("HTTP/1.1 200 OK\r\nsome body that could be anything", response.getStatusLine());
   }
 }
