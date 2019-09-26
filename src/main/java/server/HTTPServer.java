e6709b6a6161e85a65ab392df8e9d5c8c48da110
wynspeare <caroline@8thlight.com>
2019-09-26 13:13:27 -0500

    Removed acceptance-tests script and added JAR file to commit

--- src/main/java/server/HTTPServer.java
+++ src/main/java/server/HTTPServer.java
@@ -18,10 +18,17 @@ public class HTTPServer {
 
 
     public HTTPServer() {
-        serverSocket = new ServerSocketWrapper(5000);
+        this.serverSocket = new ServerSocketWrapper(5000);;
         int processingCoresAvailable = Runtime.getRuntime().availableProcessors();
-        pool = Executors.newFixedThreadPool(processingCoresAvailable);
+        this.pool = Executors.newFixedThreadPool(processingCoresAvailable);
     }
+
+//    public static void main(String[] args) {
+//        HTTPServer server = new HTTPServer();
+//        server.serverSocket = new ServerSocketWrapper(5000);;
+//        int processingCoresAvailable = Runtime.getRuntime().availableProcessors();
+//        server.pool = Executors.newFixedThreadPool(processingCoresAvailable);
+//    }
 
     public static ServerLogger createLogger(String directory) {
         return new ServerLogger(directory);
@@ -49,71 +56,3 @@ public class HTTPServer {
         serverSocket.close();
     }
 }
-
-
-//package server;
-//
-//import server.handlers.DefaultHandler;
-//import server.handlers.EchoHandler;
-//import server.handlers.RedirectHandler;
-//import server.logger.ServerLogger;
-//import server.wrappers.IServerSocketWrapper;
-//import server.wrappers.ISocketWrapper;
-//import server.wrappers.ServerSocketWrapper;
-//import java.io.IOException;
-//import java.util.concurrent.ExecutorService;
-//import java.util.concurrent.Executors;
-//
-//
-//public class HTTPServer {
-//    public IServerSocketWrapper serverSocket;
-//    private ExecutorService pool;
-//    public static final ServerLogger serverLogger = createLogger("Logs");
-//
-//    public HTTPServer(IServerSocketWrapper serverSocket, ExecutorService executorService) {
-//        this.serverSocket = serverSocket;
-//        this.pool = executorService;
-//    }
-//
-//    public static void main(String[] args) {
-//        IServerSocketWrapper serverSocketWrapper = new ServerSocketWrapper(5000);
-//        int processingCoresAvailable = Runtime.getRuntime().availableProcessors();
-//        HTTPServer server = new HTTPServer(serverSocketWrapper, Executors.newFixedThreadPool(processingCoresAvailable));
-//        Router router = createRouter();
-//        server.serve(router);
-//    }
-//
-//    public static ServerLogger createLogger(String directory) {
-//        return new ServerLogger(directory);
-//    }
-//
-//    public static Router createRouter() {
-//        Router router = new Router(serverLogger);
-//
-//        router.addRoute("GET", "/simple_get", new DefaultHandler());
-//        router.addRoute("HEAD", "/simple_get", new DefaultHandler());
-//        router.addRoute("HEAD", "/get_with_body", new DefaultHandler());
-//        router.addRoute("POST", "/echo_body", new EchoHandler());
-//        router.addRoute("GET", "/redirect", new RedirectHandler("http://127.0.0.1:5000/simple_get"));
-//        router.addRoute("GET", "/test", new DefaultHandler());
-//
-//        return router;
-//    }
-//
-//    public Runnable createRunnable(Router router) {
-//        ISocketWrapper socket = null;
-//        try {
-//            socket = serverSocket.acceptConnection();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return new ServerRunnable(socket, router);
-//    }
-//
-//    public void serve(Router router) {
-//        while (!serverSocket.isClosed()) {
-//            Thread thread = new Thread(createRunnable(router));
-//            pool.execute(thread);
-//        }
-//    }
-//}
